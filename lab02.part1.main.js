510668535dbb61c9cdec3430b84730759be74525
csanford-ctl <cody.sanford@centurylink.com>
2020-01-14 23:16:15 +0000

    Lab 2 part 2 of 2

--- lab02.part1.main.js
+++ lab02.part1.main.js
@@ -11,12 +11,12 @@ const IPCIDR = require('ip-cidr');
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                 in CIDR format.
  * @param {callback} callback - A callback function.
- * @return {object} (ipAddresses) - An object containing both the ipv4 address and ipv6 address.
+ * @return {string} (firstIpAddress) - An IPv4 address.
  */
 function getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
-  let ipAddresses = {ipv4: null, ipv6: null}
+  let firstIpAddress = null;
   let callbackError = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
@@ -36,16 +36,13 @@ function getFirstIpAddress(cidrStr, callback) {
   } else {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
-    let firstIpAddress = null;
     [firstIpAddress] = cidr.toArray(options);
-    ipAddresses.ipv4 = firstIpAddress;
-    ipAddresses.ipv6 = getIpv4MappedIpv6Address(firstIpAddress)
   }
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
   // The IAP convention is to pass returned data as the first argument and error
   // data as the second argument to the callback function.
-  return callback(ipAddresses, callbackError);
+  return callback(firstIpAddress, callbackError);
 }
 
 /**
@@ -120,7 +117,7 @@ function main() {
       if (error) {
         console.error(`  Error returned from GET request: ${error}`);
       }
-      console.log(`  Response returned from GET request: ${JSON.stringify(data)}`);
+      console.log(`  Response returned from GET request: ${data}`);
     });
   }
   // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
